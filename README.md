1491f9606c18c1ccfb6479d5c9854de7657a313e
dina-fouad <dina_albarghouthi@yahoo.com>
2021-04-13 20:39:54 +0300

    i didnt finish

---
+++ README.md
@@ -0,0 +1,5 @@
+ ## ***Project description***
+  ### ***I Create an About Me guessing game that utilizes HTML, CSS, & JavaScript. including  HTML page contains a short biography, education history, job experience, and Licenses & certifications.***
+
+  ![](https://p1xhr2w8ts37fbalioe6qfro-wpengine.netdna-ssl.com/treasuryinsights/wp-content/uploads/sites/9/2019/04/TM-3225_6501303_WSL-WCS-TM_Q219_iStock887921494-edit-img-1600x700_v3.png)
+ 
--- js/app.js
+++ js/app.js
@@ -1,61 +1,88 @@
-'use strict'
+'use strict';
 
-let inputName = prompt('whats your name?')
-   alert('Welcome to my site ,'+inputName+'') ;
+//let inputName = prompt('whats your name?')
+  // alert('Welcome to my site ,'+inputName+'') ;
 
 
-let inputLanguage = prompt ('Did i study French language?').toLocaleLowerCase() ;
+//let inputLanguage = prompt ('Did i study French language?').toLocaleLowerCase() ;
 
-if (inputLanguage === 'yes' || inputLanguage ==='y'){
-alert('Correct answer , I studied French Language & Literature from The University Of Jordan') ;
+//if (inputLanguage === 'yes' || inputLanguage ==='y'){
+//alert('Correct answer , I studied French Language & Literature from The University Of Jordan') ;
 //console.log('Correct answer , I studied French Language & Literature from The University Of Jordan') ;
-} else if (inputLanguage === 'no' || inputLanguage ==='n'){
-alert('wrong answer');
-}else {
-    alert('please enter yes or no') ;
-}
+//} else if (inputLanguage === 'no' || inputLanguage ==='n'){
+//alert('wrong answer');
+//}else {
+  //  alert('please enter yes or no') ;
+//}
 
-let input = prompt ('Did i know Hebrew?').toLocaleLowerCase() ;
+//let input = prompt ('Did i know Hebrew?').toLocaleLowerCase() ;
 
-if (input === 'yes' || input ==='y'){
-alert('Correct answer , I know little bit') ;
+//if (input === 'yes' || input ==='y'){
+//alert('Correct answer , I know little bit') ;
 //console.log('Correct answer , I know little bit') ;
-} else if (input === 'no' || input ==='n'){
-alert('wrong answer');
-}else {
-    alert('please enter yes or no') ;
-}
+//} else if (input === 'no' || input ==='n'){
+//alert('wrong answer');
+//}else {
+  //  alert('please enter yes or no') ;
+//}
 
 
-let inputWork = prompt ('Did i work in toursim company?').toLocaleLowerCase() ;
+//let inputWork = prompt ('Did i work in toursim company?').toLocaleLowerCase() ;
 
-if (inputWork === 'yes' || inputWork ==='y'){
-alert('Correct answer , I worked as a reservation agent') ;
+//if (inputWork === 'yes' || inputWork ==='y'){
+//alert('Correct answer , I worked as a reservation agent') ;
 //console.log('Correct answer , I worked as a reservation agent') ;
-} else if (inputWork === 'no' || inputWork ==='n'){
-alert('wrong answer');
-}else {
-    alert('please enter yes or no') ;
-}
+//} else if (inputWork === 'no' || inputWork ==='n'){
+//alert('wrong answer');
+//}else {
+  //  alert('please enter yes or no') ;
+//}
 
-let inputDate = prompt ('Did I graduate in 2017?').toLocaleLowerCase() ;
+//let inputDate = prompt ('Did I graduate in 2017?').toLocaleLowerCase() ;
 
-if (inputWork === 'yes' || inputWork ==='y'){
-alert('wrong answer') ;
-} else if (inputWork === 'no' || inputWork ==='n'){
-alert('Correct answer , I graduated in 2015');
+//if (inputWork === 'yes' || inputWork ==='y'){
+//alert('wrong answer') ;
+//} else if (inputWork === 'no' || inputWork ==='n'){
+//alert('Correct answer , I graduated in 2015');
 //console.log('Correct answer , I graduated in 2015') ;
-}else {
-    alert('please enter yes or no') ;
-}
+//}else {
+  //  alert('please enter yes or no') ;
+//}
 
-let inputFood = prompt ('Do i like Burger?').toLocaleLowerCase() ;
+//let inputFood = prompt ('Do i like Burger?').toLocaleLowerCase() ;
 
-if (inputFood === 'yes' || inputFood ==='y'){
-alert('wrong answer') ;
-} else if (inputFood === 'no' || inputFood ==='n'){
-alert('Correct answer , I dont like it, Thank you for answering '+inputName+'');
+//if (inputFood === 'yes' || inputFood ==='y'){
+//alert('wrong answer') ;
+//} else if (inputFood === 'no' || inputFood ==='n'){
+//alert('Correct answer , I dont like it, Thank you for answering '+inputName+'');
 //console.log('Correct answer , I dont like it, Thank you for answering '+inputName+'') ;
+//}else {
+  //  alert('please enter yes or no , Thank you for answering '+inputName+'') ;
+//}
+
+
+
+for (let i=0 ; i<4 ; i++){
+    let inputNumber = prompt('Guess the secret number in the magicians hat');
+  if(inputNumber==5){
+        alert('too high') ;
+        break
 }else {
-    alert('please enter yes or no , Thank you for answering '+inputName+'') ;
+      alert('too low ') ;
 }
+
+}
+alert ('The correct answer is 5')
+
+
+//let  arrayName= ['paris','maldive', 'istanbul','rome','london']
+//for (let y=0 ; y<6 ;y++){
+  
+  //  let inputPlace = prompt('what is the best places to visit?');
+    // if(inputPlace === arrayName ){
+       // break
+    //}else {
+      //  alert('wrong answer')
+   // }
+//}
+
